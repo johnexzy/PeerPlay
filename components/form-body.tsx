e5f3d2b429b14a0a021d9cca1096c4392566c420
John Oba <obajohn75@gmail.com>
2023-12-28 03:46:11 +0100

    :rocket: (added domain) assigned to https://peerplay.space

--- components/form-body.tsx
+++ components/form-body.tsx
@@ -15,7 +15,7 @@ export default function FormBody({ state }: { state: FormState, sources: { id: s
             <p className=" font-mono ">
               <span className="font-bold">Link</span>:{" "}
               <span className="text-green-600">
-                https://peer-play.space/play/{state.data.id}
+                https://peerplay.space/play/{state.data.id}
               </span>
             </p>
             <div className=" font-mono ">
